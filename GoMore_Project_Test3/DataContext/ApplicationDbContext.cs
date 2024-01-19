81e445b09c25d812258893005883f6db19f66d17
KY <xiaoyang7481@gmail.com>
2024-01-19 23:13:50 +0800

    加入專案檔案。

---
+++ GoMore_Project_Test3/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using GoMore_Project_Test3.Models;
+
+namespace GoMore_Project_Test3.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base(nameOrConnectionString : "MyConnection")
+        {
+        }
+        public virtual DbSet<EmpClass> Empobj { get; set; }
+    }
+}
---
+++ GoMore_Project_Test3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoMore_Project_Test3.MvcApplication" Language="C#" %>
